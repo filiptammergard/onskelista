d8699fcfe8847ce7374c54b85f1b0839c228c490
Filip Tammergard <filip@tammergard.se>
2019-10-19 22:46:48 +0200

    config

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,4 +1,12 @@
 // TODO: Fill with correct config
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBlqJ_MY-ViPGO2GPuRRz0lJbc_9kuATmU",
+  authDomain: "onskelista-nilsolga-3d092.firebaseapp.com",
+  databaseURL: "https://onskelista-nilsolga-3d092.firebaseio.com",
+  projectId: "onskelista-nilsolga-3d092",
+  storageBucket: "onskelista-nilsolga-3d092.appspot.com",
+  messagingSenderId: "1017941562218",
+  appId: "1:1017941562218:web:ad77aa1fba4a81ed1e0b13"
+};
 
 export default firebaseConfig;
